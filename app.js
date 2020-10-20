5cb34e2769536d2017cb0df016b37f872bc67cf3
GowrisGit <gowris.dev@gmail.comgit config -global user.name GowrisGitgit config --global user.email gowris.dev@gmail.com>
2020-10-20 14:57:04 +1100

    finish first Part RGB with HOME button

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "yello","violet", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+
+    console.log("Inside eventListner- ", document.body)
+
+    const randomNumber = getRandomNumber(); 
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+
+})// addEventListener
+
+function getRandomNumber(){
+    return Math.floor( Math.random()*colors.length);
+}
